3e2ea8b2b6e99188f452461e3eb6c34ad5269664
theowiik <42991003+theowiik@users.noreply.github.com>
2024-07-10 19:27:22 +0200

    add ping command

--- bot/src/bot.py
+++ bot/src/bot.py
@@ -8,8 +8,17 @@ import logging
 DISCORD_TOKEN = "DISCORD_TOKEN"
 
 
+class DevCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.hybrid_command(name="ping", description="Pong!")
+    async def ping(self, ctx: commands.Context):
+        await ctx.send("Pong!")
+
+
 class MarranBot(commands.Bot):
-    sync_commands = False
+    sync_commands = True
     """Prevent rate-limiting."""
 
     def __init__(self, command_prefix, intents):
@@ -28,6 +37,8 @@ class MarranBot(commands.Bot):
             logging.error(error, exc_info=True)
 
     async def on_command_error(self, ctx, error):
+        logging.error(f"Error in command {ctx.command}:", exc_info=error)
+
         if isinstance(error, commands.BadArgument):
             pass
         elif isinstance(error, commands.CommandNotFound):
@@ -35,12 +46,7 @@ class MarranBot(commands.Bot):
         elif isinstance(error, commands.CommandOnCooldown):
             await ctx.send(content=error)
         else:
-            logging.error(error, exc_info=True)
-            await ctx.send("Internal error. Command failed")
-
-    @commands.hybrid_command(name="ping", description="Pong!")
-    async def ping(self, ctx: commands.Context, user: discord.User):
-        await ctx.send(f"Pong! {user.mention}")
+            await ctx.send("uh oh, code brokey 💥")
 
 
 async def main() -> None:
@@ -49,16 +55,16 @@ async def main() -> None:
     logging.basicConfig(level=logging.INFO)
 
     bot = MarranBot(command_prefix="!", intents=intents)
+    await bot.add_cog(DevCog(bot))
     await bot.start(os.getenv(DISCORD_TOKEN))
 
 
 if __name__ == "__main__":
     load_dotenv()
-
     discord_token = os.getenv(DISCORD_TOKEN)
 
     if discord_token is None or discord_token.strip() == "":
-        logging.error("'DISCORD_TOKEN' environment variable must be set")
+        logging.error(f"'{DISCORD_TOKEN}' environment variable must be set")
         exit(1)
 
     asyncio.run(main())
